e9d7af0c65414af75efcd0bfce97a5e20c21374d
logeoSE <49406040+logeoSE@users.noreply.github.com>
2022-05-02 12:40:44 +0200

    all basic calculus

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,52 @@
 const randVue = new Vue({
     el: '#calculatrice',
     data: {
-        display: '0'
+        display: '0',
+        operatorClicked: false,
+        buffer: ''
+
       },
 
     methods: {
         append(number) {
             if (this.display === '0') {this.display = number}
-            else this.display = '' + this.display + number;
-        }
+            else {
+                this.display = '' + this.display + number;
+                this.operatorClicked = false;
+            }
+        },
+
+        add() {
+            console.log('addition');
+            if (this.operatorClicked == false){
+                this.display = '' + this.display + '+';
+                this.operatorClicked = true;
+            }
+        },
+
+        sub() {
+            console.log('soustraction');
+            if (this.operatorClicked == false){
+                this.display = '' + this.display + '-';
+                this.operatorClicked = true;
+            }
+        },
+
+        allClear() {
+            console.log('all clear !');
+            this.display = '0';
+            this.operatorClicked = false;
+            this.buffer = '';
+        },
+
+        multiply() {
+            console.log('multiplication');
+            if (this.operatorClicked == false){
+                this.display = '' + this.display + 'x';
+                this.operatorClicked = true;
+            }
+        },
+
+
       }
 })
