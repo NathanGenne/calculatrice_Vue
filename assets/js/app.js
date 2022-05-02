bba098b95d61ddf28e429e97704767aa34f908eb
logeoSE <49406040+logeoSE@users.noreply.github.com>
2022-05-02 13:08:07 +0200

    continuation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,14 @@
+/* ---------------- */
+/* Nathan + Antoine */
+/* ---------------- */
+
 const randVue = new Vue({
     el: '#calculatrice',
     data: {
         display: '0',
         operatorClicked: false,
-        buffer: ''
+        buffer: '',
+        lastOperator: ''
 
       },
 
@@ -21,6 +26,7 @@ const randVue = new Vue({
             if (this.operatorClicked == false){
                 this.display = '' + this.display + '+';
                 this.operatorClicked = true;
+                this.lastOperator = '+';
             }
         },
 
@@ -29,6 +35,7 @@ const randVue = new Vue({
             if (this.operatorClicked == false){
                 this.display = '' + this.display + '-';
                 this.operatorClicked = true;
+                this.lastOperator = '-';
             }
         },
 
@@ -37,6 +44,7 @@ const randVue = new Vue({
             this.display = '0';
             this.operatorClicked = false;
             this.buffer = '';
+            this.lastOperator = '';
         },
 
         multiply() {
@@ -44,7 +52,46 @@ const randVue = new Vue({
             if (this.operatorClicked == false){
                 this.display = '' + this.display + 'x';
                 this.operatorClicked = true;
+                this.lastOperator = 'x';
             }
+        },
+
+        divide() {
+            console.log('division');
+            if (this.operatorClicked == false){
+                this.display = '' + this.display + '/';
+                this.operatorClicked = true;
+                this.lastOperator = '/';
+            }
+        },
+
+        reverse() {
+            console.log('reverse');
+            let n = this.display;
+            n = n.replace(/\+|\-|\/|\x/g, '|');
+            n = n.split('|');
+            n[n.lengh-1] *= -1;
+
+            let newArr;
+            for (let i = 0; i < n.length; i++) {
+                newArr.push(n[i]);
+                if (operatorsArr[i]) newArr.push(operatorsArr[i]);
+            }
+
+            newArr = newArr.join('');
+            
+        },
+
+        percent() {
+            console.log('pourcentage');
+        },
+
+        decimal() {
+            console.log('décimal');
+        },
+
+        solve() {
+            console.log('résolution');
         },
 
 
