44f159c5eda428567ee0415fe1ff1f31f3f9c5ce
logeoSE <49406040+logeoSE@users.noreply.github.com>
2022-05-02 12:01:39 +0200

    adding numbers

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 const randVue = new Vue({
     el: '#calculatrice',
     data: {
-        
-    },
+        display: '0'
+      },
 
     methods: {
-        
-    }
+        append(number) {
+            if (this.display === '0') {this.display = number}
+            else this.display = '' + this.display + number;
+        }
+      }
 })
